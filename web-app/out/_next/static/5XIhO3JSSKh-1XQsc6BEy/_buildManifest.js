603d6cb9d492c6928c873419d12ac075a08b816f
Chris Teuschler <christeuschler@gmail.com>
2023-03-26 03:23:52 +0000

    export static files

---
+++ web-app/out/_next/static/5XIhO3JSSKh-1XQsc6BEy/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/101-1cafdb4c14bff3b4.js","static/chunks/pages/index-03b93db2c5a9abe5.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
